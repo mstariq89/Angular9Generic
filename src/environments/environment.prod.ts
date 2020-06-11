50bd34ff85bb22479de9f0604dc45382526a8c26
Tariq <mst6cob@rbeigcn.com>
2020-06-11 17:13:04 +0530

    environment prod config changes

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
   apiUrl: 'http://localhost:4000',
-  idleTimeout: 900, //In seconds 60 = 1 min
-  idleTimeoutPeriod: 300, //In seconds
-  checkIdleTimeoutInterval: 30 //In seconds
+  idleTimeout: 180, //In seconds 60 = 1 min
+  idleTimeoutPeriod: 90, //In seconds
+  checkIdleTimeoutInterval: 15 //In seconds
 };
